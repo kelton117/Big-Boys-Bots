b6e03813957fd9f622aa052193cea22d7abed5f6
kelton117 <Ethankelton02@gmail.com>
2021-11-04 12:14:44 -0500

    input box done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [state, setState] = useState({
+    forms: [],
+    newForm: {
+      shoe: 'ex Air Mag',
+      name: 'ex Marty McFly',
+      email: 'ex B2thefuture@gmail.com',
+      number: '',
+    }
+  });
+
+  function handleChange(event) {
+    setState(prevState => ({
+      ...prevState,
+      newForm: {...prevState.newForm, [event.target.name]: event.target.value}
+    }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await fetch('http://localhost:3001/api/forms', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'Application/json'
+      },
+      body: JSON.stringify(state.newForm)
+    });
+
+    getForms();
+
+    setState((prevState) => ({
+      ...prevState,
+      newForm: {
+      shoe: 'ex. Air Mag',
+      name: 'ex. Marty McFly',
+      email: 'ex. B2thefuture@gmail.com',
+      number: '',
+      }
+    }));
+  }
+
+  async function getForms() {
+    const response = await fetch('http://localhost:3001/api/forms');
+    const forms = await response.json();
+
+    setState((prevState) => ({
+      forms,
+      newForm: prevState.newForm
+    }));
+  }
+
+  useEffect(() => {
+    getForms();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h2>Big Boys Bots</h2>
+      <hr />
+      {state.forms.map((s) => (
+        <article key={s.shoe}>
+          <div className='shoe'>{s.shoe}</div> <div className='name'>{s.name}</div> <div className='email'>{s.email}</div> <div className='number'>{s.number}</div>
+        </article>
+      ))}
+      <hr />
+      <form name='input' onSubmit={handleSubmit}>
+        <label>
+          <span>SHOE</span>
+          <input name='shoe' value={state.newForm.shoe} onChange={handleChange} />
+        </label>
+        <label>
+          <span>NAME</span>
+          <input name='name' value={state.newForm.name} onChange={handleChange} />
+        </label>
+        <label>
+          <span>EMAIL</span>
+          <input name='email' value={state.newForm.email} onChange={handleChange} />
+        </label>
+        <label>
+          <span>PHONE NUMBER</span>
+          <input name='number' value={state.newForm.number} onChange={handleChange} />
+        </label>
+        <button>Add Bot</button>
+      </form>
+     
+    </section>
   );
+
 }
 
-export default App;
